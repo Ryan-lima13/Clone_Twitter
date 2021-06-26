e3535c4c012b7d790854994052bbfe57331fef2a
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-06-26 20:54:34 -0300

    Update AuthController.php

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -3,12 +3,20 @@
   namespace App\Controllers;
 
 //os recursos do miniframework
+
+
 use MF\Controller\Action;
 use MF\Model\Container;
 
 class AuthController extends Action{
     public function autenticar(){
-        
+        echo '<pre>';
+        print_r($_POST);
+        echo '</pre>';
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('email',$_POST['email']);
+        $usuario->__set('senha',$_POST['senha']);
+        $usuario->autenticar();
     }
 
 }
