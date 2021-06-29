68abd1d07b92225b821b9cfdce9cefc792138ec1
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-06-28 22:03:28 -0300

    Update AppController.php

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -20,9 +20,12 @@
         public function tweet(){
             session_start();
             if($_SESSION['id'] != '' && $_SESSION['nome'] != ''){
-                echo '<pre>';
-                print_r($_POST);
-                echo '</pre>';
+                
+                $tweet = Container::getModel('Tweet');
+                $tweet->__set('tweet',$_POST['tweet']);
+                $tweet->__set('id_usuario',$_SESSION['id']);
+                $tweet->salvar();
+               
 
             }else{
                 header('location: /?login=erro');
